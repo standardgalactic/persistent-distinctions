ef472099135fc4356b46918185fadd37da01a1ae
Cogito Ergo Sum <standardgalactic@protonmail.com>
2026-08-07 05:17:55 -0300

    Add setup.py for package installation

---
+++ setup.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+"""Setup configuration for persistent-distinctions-complex-systems."""
+
+from setuptools import setup, find_packages
+
+setup(
+    packages=find_packages(),
+)
